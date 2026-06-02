1377b3c7cb5c130a9c340423354f99235379e698
Rizwan Pasha <rizwanpashs1386@gmail.com>
2026-06-03 00:10:50 +0530

    Resolved merge conflict

--- backend/src/routes/tenant.route.js
+++ backend/src/routes/tenant.route.js
@@ -5,27 +5,15 @@ const {auth}=require('../middlewares/auth')
 const {isAdmin,isMember,isOwner}=require('../middlewares/roles.middleware')
 const {createTenant,displayAllTenants,createAdmin,addTenantMembers,updateMemberRole,getTenantMembers,deleteTenantMember,viewTenant,updateTenant,deleteTenant}=require('../controllers/tenant.controller')
 const {validate}=require('../middlewares/validate')
-<<<<<<< HEAD
-const {tenantIdParamSchema,tenantMemberParamSchema,}=require('../validations/tenant.validation')
-=======
+
 const {tenantIdParamSchema,tenantMemberParamSchema}=require('../validations/tenant.validation')
->>>>>>> 08e81da (feat(frontend): add tenant workspace routing, member project/task portals, and project/task API integration)
 
 router.use(auth)
 router.get('/',displayAllTenants) //?search,role
 router.post('/',createTenant)
 
 //owner
-<<<<<<< HEAD
-router.get('/:tenantId',isOwner,validate(tenantIdParamSchema,"params"),viewTenant)
-router.patch('/:tenantId',isOwner,validate(tenantIdParamSchema,"params"),updateTenant)
-router.delete('/:tenantId',isOwner,validate(tenantIdParamSchema,"params"),deleteTenant)
-router.post('/:tenantId/createAdmin',isOwner,validate(tenantIdParamSchema,"params"),createAdmin)
-router.post('/:tenantId/add-members',isOwner,validate(tenantIdParamSchema,"params"),addTenantMembers)
-router.get('/:tenantId/members',isOwner,validate(tenantIdParamSchema,"params"),getTenantMembers)  ///members?role=admin,member&search=rizwan&sort=-createdAt
-router.delete('/:tenantId/members/:memberId',isOwner,validate(tenantMemberParamSchema,"params"),deleteTenantMember)
-router.patch('/:tenantId/role',isOwner,validate(tenantIdParamSchema,"params"),updateMemberRole)
-=======
+
 router.get('/:tenantId',isOwner,validate(tenantIdParamSchema,'params'),viewTenant)
 router.patch('/:tenantId',isOwner,validate(tenantIdParamSchema,'params'),updateTenant)
 router.delete('/:tenantId',isOwner,validate(tenantIdParamSchema,'params'),deleteTenant)
@@ -34,6 +22,5 @@ router.post('/:tenantId/add-members',isOwner,validate(tenantIdParamSchema,'param
 router.get('/:tenantId/members',isOwner,validate(tenantIdParamSchema,'params'),getTenantMembers)  ///members?role=admin,member&search=rizwan&sort=-createdAt
 router.delete('/:tenantId/members/:memberId',isOwner,validate(tenantMemberParamSchema,'params'),deleteTenantMember)
 router.patch('/:tenantId/role',isOwner,validate(tenantIdParamSchema,'params'),updateMemberRole)
->>>>>>> 08e81da (feat(frontend): add tenant workspace routing, member project/task portals, and project/task API integration)
 
 module.exports=router
