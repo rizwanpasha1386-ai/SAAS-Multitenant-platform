36c47443f013094a4e0ba4e5a14376db68dfaba3
Rizwan Pasha <rizwanpashs1386@gmail.com>
2026-07-17 23:12:41 +0530

    Configuring http to https for deployed applications to work

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,11 +55,13 @@ async function login(req, res) {
         );
 
         // ✅ Set cookie
-        res.cookie("token", token, {
-            httpOnly: true,     // prevents JS access (security)
-            secure: false,      // true in production (HTTPS)
-            sameSite: "lax",    // or "strict"
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+       res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production"
+            ? "none"
+            : "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
         res.status(200).json({
